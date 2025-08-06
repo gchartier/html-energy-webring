d2315ef68fba6073e9ba43eaea19d0ffe8b49b17
Gabriel Chartier <gabriel@chartier.link>
2025-08-06 18:55:31 -0500

    remove www. from urls in members list

--- onionring/index.js
+++ onionring/index.js
@@ -6,7 +6,7 @@
 //this file builds the list of sites in the ring for displaying on your index page
 
 var tag = document.getElementById("index");
-regex = /^https?:\/\/|\/$/g; //strips the http(s):// and trailing slash off the urls for aesthetic purposes
+regex = /^https?:\/\/(www\.)?|\/$/g; //strips the http(s)://, www., and trailing slash off the urls for aesthetic purposes
 
 list = "";
 for (i = 0; i < sites.length; i++) {
