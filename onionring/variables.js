e2f86e52199dd590da6e6429b7a56e47058e32bf
Gabriel Chartier <gabriel@chartier.link>
2025-08-06 18:51:07 -0500

    add index site to the webring

--- onionring/variables.js
+++ onionring/variables.js
@@ -7,7 +7,7 @@
 
 //the full URLs of all the sites in the ring
 var sites = [
-    "https://html-energy-webring.github.io/",
+    "https://www.gabriel-export.earth/html-energy-webring/",
     "https://www.palmertx.net/me2/social.html",
     "https://callmecannibal.neocities.org/",
     "https://croakego.neocities.org/",
